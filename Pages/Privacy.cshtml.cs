16a804bfca13d9e4e79dccfe8c315ebb97629f5d
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-25 02:04:38 -0700

    Add files via upload

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_WEBB.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
