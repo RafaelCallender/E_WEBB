e374a944f3dc31ae5d4af63bd8f4ef46bae80318
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-24 23:59:49 -0700

    Add files via upload

---
+++ MODEL/CONTACT_US_MODEL/Contact_Us_Model01.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace E_WEBB.MODEL.CONTACT_US_MODEL
+{
+    public class Contact_Us_Model01
+    {
+        [BindProperty]
+        public string Name { get; set; } = string.Empty;
+        [BindProperty]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Message { get; set; } = string.Empty;
+        [BindProperty]
+        public string dateSent { get; set; } = string.Empty;
+        [BindProperty]
+        public string phonenumber { get; set; } = string.Empty;
+        [BindProperty]
+        public string StatusMessage { get; set; } = string.Empty;
+    }
+}
---
+++ SERVICES/EMAIL_SERVICES/Email_Services01.cs
@@ -0,0 +1,43 @@
+﻿using E_WEBB.SERVICES.SQL.SQL_SERVICES.SQL_CLIENT_SERVICES;
+using System.Net;
+using System.Net.Mail;
+
+namespace E_WEBB.SERVICES.EMAIL_SERVICES
+{
+    public class Email_Services01
+    {
+        private static Sql_Client_Services01 Sql_Client_S01 = new Sql_Client_Services01();
+        public bool send_email(string input, string input01, string input02, out string input03)
+        {
+            try
+            {
+           
+                string fromEmail ="callender.rafael92@gmail.com";
+                string appPassword = "lfki bbvf ogld qupd";
+                string to_add = "callender.rafael92@gmail.com";
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(fromEmail);
+                mail.To.Add(to_add.Trim());
+                mail.Subject = "Test";
+                mail.Body = $"Email: {input}\n" +
+                            $"Name: {input01}\n" +
+                            $"Message:{input02}\n";
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential(fromEmail.Trim(), appPassword.Trim());
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+                input03 = "message sent";
+                return true;
+            }
+            catch (Exception ex)
+            {
+                input03 = ex.Message;
+                return false;
+
+            }
+
+        }
+    }
+}
