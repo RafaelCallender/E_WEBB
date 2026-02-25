f3a65223497259abee2e33d03ba65ca6a65073ad
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-24 23:59:04 -0700

    Add files via upload

---
+++ TEMPLATE/Reggex_Template01.cs
@@ -0,0 +1,17 @@
+﻿namespace E_WEBB.TEMPLATE
+{
+    public class Reggex_Template01
+    {
+        private static string[] Regex01_ = { @"[^a-zA-Z0-9]",//special character regex
+                                             @"^[^@\s]+@[^@\s]+\.[^@\s]+$",//email regex
+                                             @"^\+?\d{1,3}?[\s.-]?\(?\d{1,4}\)?([\s.-]?\d{1,4}){2,3}$"//phone number regex
+        
+        };
+
+        public static string[] Regex01
+        {
+            get { return Regex01_; }
+            set { Regex01_ = value; }
+        }
+    }
+}
